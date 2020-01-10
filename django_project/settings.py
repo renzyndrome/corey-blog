c32fb36639e805fc29b143896e8036efc796b62d
renzyndrome <gabrielrebadulla@gmail.com>
2020-01-10 23:15:16 +0800

    fix secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '^varbypym+t!%lt-_0nigz-@f@8ws5(c_w$i9$pdk(q&rt2ww0'
+SECRET_KEY = '^varbypym+t!%lt-_0nigz-@f@8ws5(c_w$i9$pdk(q&rt2ww0'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
